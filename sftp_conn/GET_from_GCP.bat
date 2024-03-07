38ea15e54b96587d59a95d2c99c126fd4b869aea
JohnsonChang123 <62070342+JohnsonChang123@users.noreply.github.com>
2024-03-07 17:25:53 +0800

    Update GET_from_GCP.bat

--- sftp_conn/GET_from_GCP.bat
+++ sftp_conn/GET_from_GCP.bat
@@ -4,8 +4,8 @@ echo get -r  /home/pi/ar0/video ar0
 echo rm /home/pi/ar0/video
 echo bye
 
-)| sftp -i C:\Users\tony1\.ssh\id_rsa lab403jkc@35.194.132.151
+)| sftp -i C:\Users\tony1\.ssh\id_rsa lab403jkc@xxx.xxx.xx.xxx
 (
 echo sudo rm /home/pi/*
 echo exit
-)| ssh lab403jkc@35.194.132.151
+)| ssh lab403jkc@xxx.xxx.xx.xxx
